b095ff26d9d07f53406540eedd17dda2418323fe
Shafayat <mindfreakicup33@gmail.com>
2020-01-02 18:45:36 +0600

    Initial commit

---
+++ app/src/main/java/com/baldystudios/androidmvibasic/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.baldystudios.androidmvibasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
